053d6475354d5664ed4993d034aac167d214138b
Karthik Shetty <54941058+karthik-lezer@users.noreply.github.com>
2022-11-12 07:31:25 +0530

    Update Portfolio files
    Update my first portfolio site to GitHub repository.

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName("togell-bar")[0]
-const navbarLinks =document.getElementsByClassName("nav-buttons")[0]
-
-toggleButton.addEventListener('click',() => {
-    navbarLinks.classList.toggle('active')
-})
